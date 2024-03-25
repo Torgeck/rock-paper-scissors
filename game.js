d035e3400e363ed01ef88669b0faa4bc0da5def9
Torgeck <54917534+Torgeck@users.noreply.github>
2024-03-25 18:47:48 -0300

    Update game logic and add UI

--- game.js
+++ game.js
@@ -1,17 +1,21 @@
-function getPlayerChoice() {
-  let choice = prompt("Enter Rock/Paper/Scissors").toLowerCase();
-  let condition = false;
-  // Maybe verify that the word enter is correct
-  while (!condition) {
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-      condition = true;
-    } else {
-      choice = prompt("Re-enter Rock/Paper/Scissors").toLowerCase();
-    }
-  }
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scisBtn = document.querySelector(".scissors");
+const resultContainer = document.querySelector(".results");
+const score = document.createElement("span");
+const result = document.createElement("span");
+const maxScore = 5;
+let playerScore = 0,
+  compScore = 0;
 
-  return choice;
-}
+resultContainer.appendChild(result);
+resultContainer.appendChild(score);
+score.classList.add("score");
+score.textContent = `${playerScore} - ${compScore}`;
+
+rockBtn.addEventListener("click", () => playRoundWith("rock"));
+paperBtn.addEventListener("click", () => playRoundWith("paper"));
+scisBtn.addEventListener("click", () => playRoundWith("scissors"));
 
 function getComputerChoice() {
   const number = Math.floor(Math.random() * 3) + 1;
@@ -73,7 +77,7 @@ function playRound(playerChoice, computerChoice) {
 function getOutcome(result, player, computer) {
   let outcome;
 
-  if (result === 0) outcome = `It's a tie both played ${player} - ${computer}`;
+  if (result === 0) outcome = `It's a tie both played ${player}`;
   else if (result < 0) {
     outcome = `You lose ${computer} beats ${player}`;
   } else {
@@ -83,21 +87,43 @@ function getOutcome(result, player, computer) {
   return outcome;
 }
 
-function playGame() {
-  const maxRound = 5;
-  let currentRound = 1;
-  let playerChoice, computerChoice;
+function displayOutcome(outcome) {
+  result.textContent = outcome;
+}
 
-  for (currentRound; currentRound <= maxRound; currentRound++) {
-    playerChoice = getPlayerChoice();
+function updateScore(result) {
+  const score = document.querySelector(".score");
+  // Player won
+  if (result > 0) {
+    playerScore += 1;
+  } else {
+    compScore += 1;
+  }
+
+  // Check win game condition
+  if (playerScore === maxScore || compScore === maxScore) {
+    const finalScore = playerScore > compScore ? ` You won` : ` You lose`;
+    score.textContent = finalScore;
+  } else {
+    score.textContent = ` ${playerScore} - ${compScore}`;
+  }
+}
+
+function playRoundWith(playerChoice) {
+  let result,
     computerChoice = getComputerChoice();
 
-    console.log(
-      getOutcome(
-        playRound(playerChoice, computerChoice),
-        playerChoice,
-        computerChoice
-      )
-    );
+  // resets game if win condition met
+  if (playerScore >= maxScore || compScore >= maxScore) {
+    playerScore = 0;
+    compScore = 0;
+    score.textContent = ` ${playerScore} - ${compScore}`;
+  }
+
+  result = playRound(playerChoice, computerChoice);
+  displayOutcome(getOutcome(result, playerChoice, computerChoice));
+  // changes global variables
+  if (result != 0) {
+    updateScore(result);
   }
 }
